4272a489279cdc007437116a8577a773aaeb9a48
sha237 <sha237@proton.me>
2023-06-15 18:24:46 +0200

    fix

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require('cors');
+const path = require('path')
 
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 (async () => {
   const server = express();
   server.use(express.json());
   
   server.use(cors());
-  server.use('/static', express.static('images'));
+  server.use('/static', express.static(path.join(__dirname, 'images')));
+
 
   server.listen(PORT, async (err) => {
     console.log(`> Ready on localhost:${PORT} - env ${process.env.NODE_ENV}`);
